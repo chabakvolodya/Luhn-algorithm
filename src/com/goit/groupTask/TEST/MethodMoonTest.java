4ee3230582e45bc6532dfe6ae6916a5967b078dc
Chabak Volodya <chabakvolodya@gmail.com>
2016-01-31 22:14:38 +0400

    Refactory magic numbers

---
+++ src/com/goit/groupTask/TEST/MethodMoonTest.java
@@ -0,0 +1,14 @@
+package com.goit.groupTask.TEST;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Администратор on 1/27/2016.
+ */
+public class MethodMoonTest {
+
+    @org.junit.Test
+    public void testCheckNumber() throws Exception {
+
+    }
+}
